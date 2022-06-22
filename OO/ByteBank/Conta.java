4541790e801957ad818316efdf753c1d62bde4cf
Douglas da Silva <dev.douggo@gmail.com>
2022-06-22 18:30:42 -0300

    finalizando a aula 03 - definindo comportamento

--- OO/ByteBank/Conta.java
+++ OO/ByteBank/Conta.java
@@ -2,7 +2,29 @@ package OO.ByteBank;
 
 public class Conta {
     public double saldo;
-    public int agencia = 42;
+    public int agencia;
     public int numero;
     public String titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if(this.saldo <= 0 || this.saldo < valor) {
+            return false;
+        }
+        this.saldo -= valor;
+        return true;
+    }
+
+    public boolean transfere(Conta conta, double valor) {
+        if(this.saldo <= 0 || this.saldo < valor) {
+            return false;
+        }
+        this.saldo -= valor;
+        conta.deposita(valor);
+        return true;
+    }
+
 }
---
+++ OO/ByteBank/Movimentacoes.java
@@ -0,0 +1,53 @@
+package OO.ByteBank;
+
+public class Movimentacoes {
+    
+    public static void main(String[] args) {
+        System.out.println();
+        Conta conta = new Conta();
+        realizaDeposito(conta, 300);
+        System.out.println();
+        System.out.println();
+        realizaSaque(conta, 400);
+        System.out.println();
+        System.out.println();
+        realizaTransferencia(conta);
+    }
+
+    private static void realizaDeposito(Conta conta, double valor) {
+        conta.deposita(valor);
+        System.out.println("Realizado um depósito de R$ " + valor);
+        System.out.println("Saldo atual: R$ " + conta.saldo);
+    }
+
+    private static void realizaSaque(Conta conta, double valor) {
+        boolean sucesso = conta.saca(valor);
+        if (sucesso) {
+            System.out.println("Realizado um saque de R$ " + valor);    
+        } else {
+            System.out.println("Valor (R$ " + valor + ") indisponível para saque!");
+        }
+        System.out.println("Saldo atual: R$ " + conta.saldo);
+    }
+
+    private static void realizaTransferencia(Conta conta) {
+        Conta contaDois = new Conta();
+        double valor = 50;
+        boolean sucesso = conta.transfere(contaDois, valor);
+        if(sucesso) {
+            System.out.println("Transferido R$ " + valor + " da conta " + conta + " para a conta " + contaDois);
+        } else {
+            System.out.println("Não foi possível transferir R$ " + valor + " da conta " + conta + " para a conta " + contaDois);
+        }
+        System.out.println();
+        imprimeDados(conta);
+        imprimeDados(contaDois);
+    }
+
+    private static void imprimeDados(Conta conta) {
+        System.out.println("---- " + conta + " ----");
+        System.out.println("Saldo: R$ " + conta.saldo);
+        System.out.println();
+    }
+
+}
